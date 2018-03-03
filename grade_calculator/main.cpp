f0840df11de0d8b992c47635f5bd6edaf4e301fc
Kathyhan Nguyen <kathyhan19@gmail.com>
2018-03-03 13:38:33 -0800

    Create first slider and spinbox

--- grade_calculator/main.cpp
+++ grade_calculator/main.cpp
@@ -1,12 +1,33 @@
 #include "grade_calc.h"
 #include <QApplication>
 #include <QPushButton>
+#include <QLabel>
+#include <QSlider>
+#include <QSpinBox>
+#include <QHBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QPushButton *button = new QPushButton("Hello there");
-    button->show();
+
+    QWidget *window = new QWidget;
+
+    QLabel *label = new QLabel("Hw 1");
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    slider->setRange(0, 100);
+    slider->setValue(0);
+    QSpinBox *spinbox = new QSpinBox;
+    spinbox->setRange(0, 100);
+
+    QObject::connect(slider, SIGNAL(valueChanged(int)), spinbox, SLOT(setValue(int)));
+    QObject::connect(spinbox, SIGNAL(valueChanged(int)), slider, SLOT(setValue(int)));
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(label);
+    layout->addWidget(slider);
+    layout->addWidget(spinbox);
+    window->setLayout(layout);
+    window->show();
 
     return app.exec();
 }
